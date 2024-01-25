8ec7806acd9252cda599a5c754e60ea9330e60c5
iamshaveenvf <shaveenfernando1@gmail.com>
2024-01-25 18:04:40 +0530

    create signin api

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcrypt'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res, next)=>{
     const {username,email, password} = req.body
@@ -28,6 +29,50 @@ export const signup = async (req,res, next)=>{
     }catch(error){
         next(error)
     }
+}
+
+
+
+export const signin = async(req,res,next)=>{
+    const {email, password} = req.body;
+
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username === '' ||
+        email === '' ||
+        password === ''
+      ) {
+        next(errorHandler(400,'Empty fields'))
+      }
+
+    
+try{
+    const validUser = await User.findOne({email})
+    if(!validUser){
+        return next(errorHandler(404,'User not found'))
+    }
+
+    const validPassword = bcrypt.compareSync(password,user.password)
+
+    if(!validPassword){
+        return next(errorHandler(404, 'Password does not match'))
+    }
+
+    const token  = jwt.sign(
+        {id: validUser._id,}, 
+        process.env.JWT_SECRET,
+    )
+
+    res.status(200).cookie('access_token', token, {
+        httpOnly:true
+    }).json(validUser)
+ 
+}catch(err){
+    next(err)
+}
+    
 }
 
 
